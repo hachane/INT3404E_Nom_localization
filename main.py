a22bba651a7c1958df11d6bd1f09204d9f604075
Tăng Vĩnh Hà <tangha2005@gmail.com>
2024-05-22 21:00:55 +0700

    change main.py

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 from ultralytics import YOLO
 import os
 
-def grading(model, image_directory, output_directory):
+def grading_v2(model, image_directory, output_directory):
     for filename in os.listdir(image_directory):
         if filename.endswith('.jpg') or filename.endswith('.png'):
             results = model.predict(os.path.join(image_directory, filename), save=True, stream=True)
             txt_filename = os.path.join(output_directory, os.path.splitext(filename)[0] + '.txt')
-            
             # Check if results have any detections
             detections = False
-            for result in results:
-                if len(result.boxes) > 0:
-                    detections = True
-                    result.save_txt(txt_filename)
-                    break
-            
-            # If no detections, create an empty txt file
-            if not detections:
-                with open(txt_filename, 'w') as f:
+            with open(txt_filename, 'w') as f:
+                for result in results:
+                    if len(result.boxes) > 0:
+                        detections = True
+                        boxes = result.boxes  
+                        conf_scores = boxes.conf
+                        classes = boxes.cls
+                        w_orig, h_orig = result.orig_shape
+                        print("width img:", w_orig)
+                        print("h img:", h_orig)
+                        for i in range(len(boxes)):
+                            x1, y1, x2, y2 = boxes[i].xywh[0].cpu().numpy().astype(int)
+                            print("Original res not scaled:", x1, y1, x2, y2)
+                            cls = int(classes[i])
+                            x1_norm = x1 / h_orig
+                            x2_norm = x2 / h_orig
+                            y1_norm = y1 / w_orig
+                            y2_norm = y2 / w_orig
+                            f.write(f"{cls} {x1_norm:.4f} {y1_norm:.4f} {x2_norm:.4f} {y2_norm:.4f} {conf_scores[i]:.4f}\n")
+                # Empty file if no detections (consistency with previous behavior)
+                if not detections:
                     f.write('')
 
 if __name__ == "__main__":
@@ -26,4 +37,4 @@ if __name__ == "__main__":
     output_directory = 'FINAL_test/labels/predict'
 
     os.makedirs(output_directory, exist_ok=True)
-    grading(image_directory, output_directory)
+    grading_v2(image_directory, output_directory)
